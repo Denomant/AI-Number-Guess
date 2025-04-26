bd734fb8052fc021c1b2742574067bef1e15d148
Denomant <denomant.yt@gmail.com>
2025-04-25 22:47:44 -0400

    Layer and WeightMatrix basic functionality

--- ai.py
+++ ai.py
@@ -18,7 +18,7 @@ ReLU(values)
 """
 
 import numpy as np
-from typing import Callable
+from typing import Callable, Union, Tuple
 
 
 ActivationFunction = Callable[[np.ndarray], np.ndarray] # Function that can be considered as an activation function
@@ -28,24 +28,93 @@ class Layer:
     One layer in a neural network
     """
     def __init__(self, size : int, activation_function : ActivationFunction):
-        # TODO
-        pass
+        self.size = size
+        self.activation_function = activation_function
+        # Field initialization for later use
+        self.neurons = np.zeros(size)
+
+    def activate(self, raw_input : np.ndarray) -> None:
+        """
+        Store the input in the layer after applying the activation function on it
+        """
+        if len(raw_input) != self.size:
+            raise ValueError(f"Input size {len(raw_input)} does not match layer size {self.size}")
+        # Activation function are designed for the whole layers, rather than element by element
+        self.neurons = self.activation_function(raw_input)
+
+    def clear(self):
+        """
+        Clear the layer from any previous input, for further use
+        """
+        self.neurons = np.zeros(self.size)
+
 
 class WeightMatrix:
     """
     Handles weights between 2 layers
     """
-    def __init__(self, layer1 : Layer, layer2 : Layer):
-        # TODO
-        pass
+    def __init__(self, source_layer : Layer, target_layer : Layer):
+        # Initialize the weights with random float values between -1 and 1
+        self.weights = np.random.uniform(-1, 1, (source_layer.size, target_layer.size))
+
+    def __getitem__(self, index: Union[int, Tuple[int, int]]) -> Union[np.ndarray, float]:
+        """
+        Get all the weights for a specific neuron in layer 1, or the specific weight between 2 neurons (one from layer1 and the second layer2)
+        For index type int - returns np.ndarray of weights
+        For index type tuple - returns a float number
+        """
+        if isinstance(index, int):
+            # if index is listed as an integer, all weights for a specific Neuron will be returned in layer1, e.g object[index1]
+            # Also supports chained indexing, e.g object[index1][index2], here is how:
+            # Matrix[index1] will return the np.ndarray, and then the [index2] will let numpy handle indexing inside the ndarray
+            return self.weights[index]
+
+        # if complex index is listed as a tuple, e.g object[index1, index2]
+        elif isinstance(index, tuple):
+            # Avoiding TypeError from len() if index is not a tuple, and checks that all elements are valid indexes
+            if len(index) == 2 and all(isinstance(i, int) for i in index):
+                # Looks directly in the ndarray for the specific weight
+                return self.weights[index]
+            raise IndexError("Index must be a tuple of 2 ints")
+
+        else:
+            raise IndexError("Index must be an int, or tuple of 2 ints")
+
+    def __setitem__(self, index, value):
+        """
+        Setting new values in the weight matrix, the new value should be a numeric value (float or int).
+        Sets new value for the weight between neuron index1 in layer1 and neuron index2 in layer2
+
+        Supports:
+        - Object[index1, index2] = NewValue
+        - Object[index1][index2] = NewValue
+        """
+        # Check value first
+        if not isinstance(value, (float, int)):
+            raise ValueError("Value must be a float or an int number")
+        # In cases of [1][2] the __getitem__ will be called first, and then numpy will handle the new value assignment
+        if isinstance(index, tuple):
+            # Avoiding TypeError from len() if index is not a tuple, and checks that all elements are valid indexes
+            if len(index) == 2 and all(isinstance(i, int) for i in index):
+                # Looks directly in the ndarray for the specific weight
+                self.weights[index] = value
+            else:
+                raise IndexError("Index must be a tuple of 2 ints")
+        else:
+            raise IndexError("Index must be a tuple of 2 ints")
+            # in case of single int, __getitem__ will be called instead
+
 
 class NeuralNetwork:
     """
-    Handles multiple layers and interconnects them using multiple WeightMatrix objects
+    Handles multiple layers and interconneweightscts them using multiple WeightMatrix objects
     """
     # TODO: learning rate annealing - Decrease the learning rate as the training progresses
     def __init__(self, layer_sizes : list[int], layer_types : list[ActivationFunction]):
+        # TODO: Chenge to if - raise statement
         assert len(layer_sizes) == len(layer_types), "Layer sizes and types must be the same length"
+        # TODO
+        pass
 
 
 def BinaryStep(values : np.ndarray) -> np.ndarray:
